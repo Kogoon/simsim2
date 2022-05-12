67e91eb5e77b2571860dd8e1ff8fb0359629ae60
Kogoon <gojs712@gmail.com>
2022-05-12 00:32:22 +0000

    Modified settings.py adding apps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -51,9 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    '',
-    '',
-    '',
+    'lottoapp',
+    'menuapp',
 ]
 
 MIDDLEWARE = [
